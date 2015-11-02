ef6987f28560b95a84ee3e833e05cf886db1df06
Rabbi Moshe Plotkin <mplotkin@jewpaltz.com>
2015-11-02 15:44:55 +0000

    ajax

---
+++ php/Views/persons/edit.php
@@ -0,0 +1,5 @@
+<form>
+    <input type="text" name="Name" class="form-control" placeholder="Name" value="<?=$model['Name']?>" />
+    <input type="text" name="Birthday" class="form-control" placeholder="Birthday" value="<?=$model['Birthday']?>" />
+    <input type="submit" value="Submit" class="btn btn-primary"/>
+</form>
--- php/Views/shared/_Template.php
+++ php/Views/shared/_Template.php
@@ -13,6 +13,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   </head>
   <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
       
     <div class="container">
         <h1>My Website</h1>
@@ -56,7 +58,5 @@
             <?php include __DIR__ . "/../$view"; ?>
     </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   </body>
 </html>
